7feb918362d2f058f91a956ff74032d73dc871b6
musmana <shinymusmana@gmail.com>
2025-10-18 03:25:35 +0530

    Fix: Vercel build command

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // ✅ or remove completely — don't use /kids-reading-book/ on Vercel
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
 })
