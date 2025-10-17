5c6e74beaac32ba37f30f77ca48c3fa10c0d4c41
musmana <shinymusmana@gmail.com>
2025-10-18 03:02:04 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <Router basename="/">
+ <Router basename="/kids-reading-book">
       <div className="flex flex-col min-h-screen bg-navy text-white">
         {/* Navbar always at top */}
         <Navbar />
